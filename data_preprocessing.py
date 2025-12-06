96c4d8e645d6a75ff40b7f27a2941d503dc3eedc
Mateusz Cierpik <mateuszcierpik@gmail.com>
2025-12-06 17:21:18 +0100

    sample data + map_points function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,6 +4,7 @@ from rosbags.highlevel import AnyReader
 import open3d as op3
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 class DataPreprocessing:
     def __init__(self):
@@ -43,8 +44,8 @@ class DataPreprocessing:
                            [0.0, 0.0, 1.0]])
         
         
-        self.tracks = np.load("E:/GSN/train/interlaken_00_c/object_detections/left/tracks.npy")
-        self.exposures = self.load_image_timestamps("interlaken_00_c_image_exposure_timestamps_left.txt") 
+        self.tracks = np.load("object_detections/interlaken_00_c/left/tracks.npy")
+        self.exposures = self.load_image_timestamps("object_detections/interlaken_00_c/left/interlaken_00_c_image_exposure_timestamps_left.txt") 
         
     def load_image_timestamps(self, file_name):
         timestamps = []
@@ -68,7 +69,7 @@ class DataPreprocessing:
         return xyzit
         
     def prepare_seq(self, seq_path, out_dir):
-        self.frame_id = 0
+        frame_id = 0    
         with AnyReader([seq_path]) as reader:
             for conn, timestamp, raw in reader.messages():
                 if conn.topic == '/velodyne_points':
@@ -79,91 +80,93 @@ class DataPreprocessing:
                     # print(msg.data.shape)
 
                     pts = self.pointcloud2_to_xyzit(msg)
-                    filename = out_dir / f"frame_{self.frame_id:06d}.npy"
-                    #np.save(filename, pts)
+                    filename = out_dir / f"frame_{frame_id:06d}.npy"
+                    np.save(filename, pts)
                     
-                    # W = 640 #1440
-                    # H = 480 #1080
-                    #img = np.zeros((H,W,3))
-                    #img = cv2.imread(f"E:/GSN/imgs/{self.frame_id:06d}.png")
-                    
-                    img = cv2.imread(f"E:/GSN/imgs_/train/interlaken_00_c/images/left/distorted/{self.frame_id:06d}.png")
-                    H, W = img.shape[:2]
-                    
-                    xyz = pts[:, :3]
-                    pcd = op3.geometry.PointCloud()
-                    pcd.points = op3.utility.Vector3dVector(xyz)
-                    op3.visualization.draw_geometries([pcd])
+                    frame_id += 1
+          
+        
+    def map_points(self, points_dir):
+        for frame_id, name in enumerate(os.listdir(points_dir)):   
+            pts = np.load(os.path.join(points_dir, name))
 
-                    plt.figure(figsize=(8,8))
-                    plt.scatter(pts[:, 0], pts[:, 1], s=0.2)
-                    plt.xlabel("X")
-                    plt.ylabel("Y")
-                    plt.title("widok z gory")
-                    plt.axis("equal")
-                    
-                    ones = np.ones((xyz.shape[0], 1))
-                    pts_h = np.hstack([xyz, ones])
-                    pts_cam = (self.T_cam_lidar @ pts_h.T).T
-                    
-                    X, Y, Z = pts_cam[:, 0], pts_cam[:, 1], pts_cam[:, 2]
-                    mask = Z > 0
-                    X, Y, Z = X[mask], Y[mask], Z[mask]
-                    
-                    plt.figure(figsize=(8,8))
-                    plt.scatter(X, Z, s=0.2)
-                    plt.xlabel("X")
-                    plt.ylabel("Z")
-                    plt.title("cam view")
-                    plt.axis("equal")
-                    
-                    plt.figure(figsize=(8,8))
-                    plt.scatter(X, Y, s=0.2)
-                    plt.xlabel("X")
-                    plt.ylabel("Y")
-                    plt.title("cam view 2")
-                    plt.axis("equal")
-                    
-                    
-                    u = self.K[0,0] * X / Z + self.K[0,2]
-                    v = self.K[1,1] * Y / Z + self.K[1,2]
-                    
-                    mask_img = (u >= 0) & (u < W) & (v >=0) & (v < H)
-                    u = u[mask_img].astype(int)
-                    v = v[mask_img].astype(int)
-                    for px, py in zip(u, v):
-                        cv2.circle(img, (px, py), 2, (0,255,0), -1)
-                        
-                        
-                        
-                    ## bboxy 
-                    mask = (self.tracks["t"] >= self.exposures[self.frame_id][0]) & (self.tracks["t"] <= self.exposures[self.frame_id][1])    
-                    frame_tracks = self.tracks[mask]    
-                    for det in frame_tracks:
-                        x = int(det["x"])
-                        y = int(det["y"])
-                        w = int(det["w"])
-                        h = int(det["h"])
-                        cls = int(det["class_id"])
-                        track = int(det["track_id"])
-                        color = (0, 0, 255)
+            img = cv2.imread(f"images_raw/{frame_id:06d}.png")
+            img_distorted = cv2.imread(f"images_distorted/{frame_id:06d}.png")
+            H, W = img.shape[:2]
+            
+            xyz = pts[:, :3]
+            pcd = op3.geometry.PointCloud()
+            pcd.points = op3.utility.Vector3dVector(xyz)
+            op3.visualization.draw_geometries([pcd])
 
-                        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-                        cv2.putText(img, f"{cls}", (x, y - 5),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-                    
-                    cv2.imshow("results", img)
-                    cv2.waitKey(0)
-                    plt.show()
-                    
-                    self.frame_id += 1
-                    
-                    
-                    
+            plt.figure(figsize=(8,8))
+            plt.scatter(pts[:, 0], pts[:, 1], s=0.2)
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.title("widok z gory [raw]")
+            plt.axis("equal")
+            plt.show()
+            
+            ones = np.ones((xyz.shape[0], 1))
+            pts_h = np.hstack([xyz, ones])
+            pts_cam = (self.T_cam_lidar @ pts_h.T).T
+            
+            X, Y, Z = pts_cam[:, 0], pts_cam[:, 1], pts_cam[:, 2]
+            #X, Y, Z = pts_cam[:, 0], -pts_cam[:, 1], pts_cam[:, 2]
+            
+            mask = Z > 0
+            X, Y, Z = X[mask], Y[mask], Z[mask]
+            
+            
+            u = self.K[0,0] * X / Z + self.K[0,2]
+            v = self.K[1,1] * Y / Z + self.K[1,2]
+            
+            mask_img = (u >= 0) & (u < W) & (v >=0) & (v < H)
+            u = u[mask_img].astype(int)
+            v = v[mask_img].astype(int)                
+                
+            fig, axs = plt.subplots(2, 2, figsize=(12,12))
+            axs[0,0].scatter(X, Y, s=1)
+            axs[0,0].set_title("X Y")
+            axs[0,0].axis("equal")
 
-        #print(self.frame_id)
-        #print(pts[0])
-        #print(pts.shape)
+            axs[0,1].scatter(X, Z, s=1)
+            axs[0,1].set_title("X Z")
+            axs[0,1].axis("equal")
+
+            axs[1,0].scatter(u, v, s=1)
+            axs[1,0].set_title("u v")
+
+            img2 = img.copy()
+            for px, py in zip(u, v):
+                cv2.circle(img2, (px, py), 1, (0,255,0), -1)
+            axs[1,1].imshow(img2[:,:,::-1])
+            axs[1,1].set_title("cam view")
+            
+            plt.tight_layout()
+            plt.show()                        
+                
+            ## bboxy 
+            mask = (self.tracks["t"] >= self.exposures[frame_id][0]) & (self.tracks["t"] <= self.exposures[frame_id][1])    
+            frame_tracks = self.tracks[mask]    
+            for det in frame_tracks:
+                x = int(det["x"])
+                y = int(det["y"])
+                w = int(det["w"])
+                h = int(det["h"])
+                cls = int(det["class_id"])
+                track = int(det["track_id"])
+                color = (0, 0, 255)
+
+                cv2.rectangle(img_distorted, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img_distorted, f"{cls}", (x, y - 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            
+            cv2.imshow("results", img_distorted)
+            cv2.waitKey(0)
+            plt.show()
+                    
+                    
         
     def visualization(self, seq_path):
         vis = op3.visualization.Visualizer()
@@ -196,12 +199,14 @@ class DataPreprocessing:
         vis.destroy_window()
 
 
-seq_path_1 = Path(r"E:/GSN/data/interlaken_00/lidar_imu.bag")
+#seq_path_1 = Path(r"E:/GSN/data/interlaken_00/lidar_imu.bag")
 #seq_path_1 = Path(r"E:/GSN/data/zurich_city_00/lidar_imu.bag")
 out_dir = Path(r"frames_00")
 out_dir.mkdir(parents=True, exist_ok=True)
   
 dp = DataPreprocessing()
-dp.prepare_seq(seq_path_1, out_dir)    
+#dp.prepare_seq(seq_path_1, out_dir)    
 #dp.visualization(seq_path_1)
+
+dp.map_points(out_dir)
 
